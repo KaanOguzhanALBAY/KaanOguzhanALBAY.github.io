23cc6906fdfd5a196c5c502253df7c7b40fefbb7
KaanOguzhanALBAY <kaanoguzhanalbay@gmail.com>
2025-07-29 09:07:17 +0300

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,8 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  site : 'https://KaanOguzhanALBAY.github.io', base: 'my-repo'
   vite: {
     plugins: [tailwindcss()],
-  },
+  }, 
 });
